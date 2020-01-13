99869f6162a27b78bf5880595b9c87d91a91703a
dimin0514 <dimin0514@gmail.com>
2020-01-14 01:25:43 +0900

    first commit

---
+++ src/main/java/com/catdog/web/Sbv3Application.java
@@ -0,0 +1,13 @@
+package com.catdog.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbv3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbv3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/catdog/web/Sbv3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catdog.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbv3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
